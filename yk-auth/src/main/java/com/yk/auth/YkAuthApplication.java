9faeb7776355b697875e8f903d3d8dc44ef52b3f
yzw <ziwei@carbonstop.net>
2022-01-10 20:18:06 +0800

    yk-auth

--- yk-auth/src/main/java/com/yk/auth/YkAuthApplication.java
+++ yk-auth/src/main/java/com/yk/auth/YkAuthApplication.java
@@ -5,11 +5,13 @@ package com.yk.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 认证授权中心
  */
 //@EnableRyFeignClients
+@EnableFeignClients
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class YkAuthApplication {
 
