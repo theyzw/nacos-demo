e6c34a646cb67cd3a31c410b74c6139b91cd7b2d
yzw <ziwei@carbonstop.net>
2022-01-10 20:11:46 +0800

    yk-auth

--- yk-gateway/src/main/java/com/yk/gateway/YkGatewayApplication.java
+++ yk-gateway/src/main/java/com/yk/gateway/YkGatewayApplication.java
@@ -1,13 +1,16 @@
 package com.yk.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class YkGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(YkGatewayApplication.class, args);
+        log.info("网关启动成功");
     }
 
 }
