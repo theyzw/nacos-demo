4a0e7775b5260f5de259ccfb8735989d13afd8bb
yzw <ziwei@carbonstop.net>
2022-01-11 17:09:44 +0800

    yk-common-security

--- yk-auth/src/main/java/com/yk/auth/controller/TokenController.java
+++ yk-auth/src/main/java/com/yk/auth/controller/TokenController.java
@@ -1,11 +1,15 @@
 package com.yk.auth.controller;
 
+import com.google.common.collect.Sets;
 import com.yk.auth.request.LoginReq;
 import com.yk.auth.response.TokenResp;
 import com.yk.auth.service.SysLoginService;
 import com.yk.common.core.domain.ApiResult;
+import com.yk.common.core.text.Convert;
 import com.yk.security.model.LoginUser;
+import com.yk.security.service.TokenService;
 import io.swagger.annotations.Api;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -23,22 +27,39 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 @Api(tags = "token")
 public class TokenController {
-//    @Autowired
-//    private TokenService tokenService;
+
+    @Autowired
+    private TokenService tokenService;
 
     @Autowired
     private SysLoginService sysLoginService;
 
     @PostMapping("login")
     public ApiResult<TokenResp> login(@RequestBody LoginReq req) {
+        log.info("登录");
         // 用户登录
-        LoginUser userInfo = sysLoginService.login(req.getUsername(), req.getPassword());
+        LoginUser loginUser = sysLoginService.login(req.getUsername(), req.getPassword());
+
+        loginUser.setUserid(99L);
+        loginUser.setUsername("admin");
+        loginUser.setRoles(Sets.newHashSet("a", "b"));
+        loginUser.setPermissions(Sets.newHashSet("aa", "bb", "cc"));
         // 获取登录token
+        Map<String, Object> tokenMap = tokenService.createToken(loginUser);
         // 接口返回信息
-        TokenResp resp = TokenResp.builder().accessToken("adsfd").expiresIn(111L).build();
+        TokenResp resp = TokenResp.builder()
+            .accessToken(Convert.toStr(tokenMap.get("access_token")))
+            .expiresIn(Convert.toLong(tokenMap.get("expires_in")))
+            .build();
 
         return ApiResult.ok(resp);
-//        return R.ok(tokenService.createToken(userInfo));
+    }
+
+    @PostMapping("logout")
+    public ApiResult logout() {
+//        String token = SecurityUtils.getToken(request);
+
+        return ApiResult.ok();
     }
 
 //    @DeleteMapping("logout")
---
+++ yk-common/yk-common-security/src/main/java/com/yk/security/service/TokenService.java
@@ -0,0 +1,148 @@
+package com.yk.security.service;
+
+import cn.hutool.core.lang.UUID;
+import com.google.common.collect.Maps;
+import com.yk.common.core.constants.CacheConstants;
+import com.yk.common.core.constants.SecurityConstants;
+import com.yk.common.core.utils.IpUtils;
+import com.yk.common.core.utils.JwtUtils;
+import com.yk.common.core.utils.ServletUtils;
+import com.yk.common.core.utils.StringUtils;
+import com.yk.common.redis.utils.RedisUtil;
+import com.yk.security.model.LoginUser;
+import com.yk.security.utils.SecurityUtils;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+/**
+ * token验证处理
+ *
+ * @author ruoyi
+ */
+@Component
+public class TokenService {
+
+    protected static final long MILLIS_SECOND = 1000;
+
+    protected static final long MILLIS_MINUTE = 60 * MILLIS_SECOND;
+
+    private final static long expireTime = CacheConstants.EXPIRATION;
+
+    private final static String ACCESS_TOKEN = CacheConstants.LOGIN_TOKEN_KEY;
+
+    private final static Long MILLIS_MINUTE_TEN = CacheConstants.REFRESH_TIME * MILLIS_MINUTE;
+
+    /**
+     * 创建令牌
+     */
+    public Map<String, Object> createToken(LoginUser loginUser) {
+        String token = UUID.fastUUID().toString();
+
+        loginUser.setToken(token);
+        loginUser.setIpaddr(IpUtils.getIpAddr(ServletUtils.getRequest()));
+        refreshToken(loginUser);
+
+        // Jwt存储信息
+        Map<String, Object> claimsMap = Maps.newHashMap();
+        claimsMap.put(SecurityConstants.USER_KEY, token);
+        claimsMap.put(SecurityConstants.DETAILS_USER_ID, loginUser.getUserid());
+        claimsMap.put(SecurityConstants.DETAILS_USERNAME, loginUser.getUsername());
+
+        // 接口返回信息
+        Map<String, Object> rspMap = Maps.newHashMap();
+        rspMap.put("access_token", JwtUtils.createToken(claimsMap));
+        rspMap.put("expires_in", expireTime);
+        return rspMap;
+    }
+
+    /**
+     * 获取用户身份信息
+     *
+     * @return 用户信息
+     */
+    public LoginUser getLoginUser() {
+        return getLoginUser(ServletUtils.getRequest());
+    }
+
+    /**
+     * 获取用户身份信息
+     *
+     * @return 用户信息
+     */
+    public LoginUser getLoginUser(HttpServletRequest request) {
+        // 获取请求携带的令牌
+        String token = SecurityUtils.getToken(request);
+        return getLoginUser(token);
+    }
+
+    /**
+     * 获取用户身份信息
+     *
+     * @return 用户信息
+     */
+    public LoginUser getLoginUser(String token) {
+        LoginUser user = null;
+        try {
+            if (StringUtils.isNotEmpty(token)) {
+                String userkey = JwtUtils.getUserKey(token);
+                user = RedisUtil.get(getTokenKey(userkey));
+                return user;
+            }
+        } catch (Exception e) {
+        }
+        return user;
+    }
+
+    /**
+     * 设置用户身份信息
+     */
+    public void setLoginUser(LoginUser loginUser) {
+        if (StringUtils.isNotNull(loginUser) && StringUtils.isNotEmpty(loginUser.getToken())) {
+            refreshToken(loginUser);
+        }
+    }
+
+    /**
+     * 删除用户缓存信息
+     */
+    public void delLoginUser(String token) {
+        if (StringUtils.isNotEmpty(token)) {
+            String userkey = JwtUtils.getUserKey(token);
+            RedisUtil.del(getTokenKey(userkey));
+        }
+    }
+
+    /**
+     * 验证令牌有效期，相差不足120分钟，自动刷新缓存
+     *
+     * @param loginUser
+     */
+    public void verifyToken(LoginUser loginUser) {
+        long expireTime = loginUser.getExpireTime();
+        long currentTime = System.currentTimeMillis();
+        if (expireTime - currentTime <= MILLIS_MINUTE_TEN) {
+            refreshToken(loginUser);
+        }
+    }
+
+    /**
+     * 刷新令牌有效期
+     *
+     * @param loginUser 登录信息
+     */
+    public void refreshToken(LoginUser loginUser) {
+        loginUser.setLoginTime(System.currentTimeMillis());
+        loginUser.setExpireTime(loginUser.getLoginTime() + expireTime * MILLIS_MINUTE);
+        // 根据uuid将loginUser缓存
+        String userKey = getTokenKey(loginUser.getToken());
+
+        RedisUtil.set(userKey, loginUser, expireTime, TimeUnit.MINUTES);
+    }
+
+    private String getTokenKey(String token) {
+        return ACCESS_TOKEN + token;
+    }
+}
